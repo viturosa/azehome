2f71a9a115e5bc416b898d466bfd4de3f1bb676b
Vitor Rosa <joaovhp@hotmail.com>
2025-04-07 13:05:16 -0300

    imageUrl2AddedDetailId1

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -7,51 +7,65 @@ import './style.css'
 const Detail = () => {
   const { id } = useParams()
   const [product, setProduct] = useState(null)
+	const [currentImage, setCurrentImage] = useState("")
 
 	useEffect(() => {
 		const fetchProduct = async () => {
 			try {
-				console.log("Buscando produto com ID:", id);
+				console.log("Buscando produto com ID:", id)
 
-				const categoriesSnapshot = await getDocs(collection(db, "categories"));
+				const categoriesSnapshot = await getDocs(collection(db, "categories"))
 
 				for (const categoryDoc of categoriesSnapshot.docs) {
-					console.log("Verificando categoria:", categoryDoc.id);
+					console.log("Verificando categoria:", categoryDoc.id)
 
 
-					const categoryData = categoryDoc.data();
+					const categoryData = categoryDoc.data()
 
 
 					if (categoryData.items) {
 
-						const foundProduct = categoryData.items.find(item => item.id === Number(id));
+						const foundProduct = categoryData.items.find(item => item.id === Number(id))
 
 						if (foundProduct) {
-							console.log("Produto encontrado:", foundProduct);
-							setProduct(foundProduct);
+							console.log("Produto encontrado:", foundProduct)
+							setProduct(foundProduct)
+							setCurrentImage(foundProduct.imageUrl)
 							return;
 						}
 					}
 				}
-				console.log("Produto não encontrado.");
+				console.log("Produto não encontrado.")
 			} catch (error) {
-				console.error("Erro ao buscar produto:", error);
+				console.error("Erro ao buscar produto:", error)
 			}
-		};
+		}
 
-		fetchProduct();
-	}, [id]);
+		fetchProduct()
+	}, [id])
+	const toggleImage = () => {
+    if (!product) return
+
+    setCurrentImage(prev =>
+      prev === product.imageUrl ? product.imageUrl2 : product.imageUrl
+    )
+  }
 
   if (!product) return <p>Carregando...</p>
 
   return (
     <div className='product-detail'>
       <h1>{product.name}</h1>
-      <img src={product.imageUrl} alt={product.name} />
+			<img src={currentImage} alt={product.name} />
+
+<div className='img-selector'>
+	<button className='img-square' onClick={toggleImage}></button>
+	<button className='img-square' onClick={() => setCurrentImage(product.imageUrl)}></button>
+</div>
       <p>{product.description}</p>
       <p><strong>Preço:</strong> R$ {product.price.toFixed(2)}</p>
     </div>
   )
 }
 
-export default Detail;
+export default Detail
